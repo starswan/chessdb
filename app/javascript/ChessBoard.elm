e17919ecae8d3308e1458fc1b45e86029b9b83c1
starswan <github@starswan.com>
2024-05-11 09:20:15 +0100

    added start of ChessBoard

---
+++ app/javascript/ChessBoard.elm
@@ -0,0 +1,20 @@
+module ChessBoard exposing (..)
+
+import Square exposing (Square)
+import SquareFile
+import SquareRank exposing (SquareRank)
+
+
+type alias ChessBoard =
+    { squares : List (List Square)
+    }
+
+
+start : ChessBoard
+start =
+    ChessBoard (SquareRank.all |> List.map makeRow)
+
+
+makeRow : SquareRank -> List Square
+makeRow rank =
+    SquareFile.all |> List.map (\col -> Square.make col rank)
