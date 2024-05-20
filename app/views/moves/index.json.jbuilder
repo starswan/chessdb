8da161fc67fc1862a732b21daf9357ff593b6bb5
Stephen Dicks <github@starswan.com>
2024-05-20 19:24:38 +0100

    Fix up moves API JS call (#141)

--- app/views/moves/index.json.jbuilder
+++ app/views/moves/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@moves) do |move|
-  json.extract! move, :id, :game_id, :number, :colour, :fen
-  json.url move_url(move, format: :json)
+  json.extract! move, :move, :from, :to, :piece, :number, :fen
+  # json.url move_url(move, format: :json)
 end
--- spec/features/openings_spec.rb
+++ spec/features/openings_spec.rb
@@ -1,16 +1,14 @@
 require "rails_helper"
 
-RSpec.describe "root path", :js, type: :feature do
+RSpec.describe "root path", type: :feature do
   before do
-    # create(:opening)
     create(:game, opening: build(:opening))
   end
 
   let(:game) { Game.last }
 
-  it 'can visit the root', :js do
+  it 'can visit the root' do
     visit '/'
-    sleep 2
 
     expect(page).to have_content 'Openings'
     expect(page).to have_content game.opening.ecocode
