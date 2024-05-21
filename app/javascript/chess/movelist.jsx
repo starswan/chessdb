091fe173db28990b2b8c771be2c2b3c609ae408b
starswan <github@starswan.com>
2024-05-21 21:56:18 +0100

    react version of move folding

--- app/javascript/chess/movelist.jsx
+++ app/javascript/chess/movelist.jsx
@@ -1,6 +1,17 @@
 import React, {useEffect} from 'react'
 import {useState} from 'react';
 
+function fold_moves(listwithspare, target) {
+    const spare = listwithspare.spare;
+    if (spare === null) {
+        return { list: listwithspare.list, spare: target }
+    } else {
+        const newitem = {number: spare.number, white: spare.move, black: target.move };
+        listwithspare.list.push(newitem);
+        return { list: listwithspare.list, spare: null }
+    }
+}
+
 export function Movelist({game, moves_url}) {
     const [getgame, setGame] = useState(game);
     const [moves, setMoves] = useState([])
@@ -11,16 +22,19 @@ export function Movelist({game, moves_url}) {
             })
             .then((data) => {
                 console.log(data);
-                setMoves(data);
+                const folded = data.reduce(fold_moves, { list: [], spare: null })
+                setMoves(folded.list);
             });
     }, []);
 
+    const move_list = moves.map((move) => {
+        const item = move.white + ' ' + move.black;
+
+        return <li key={move.number}>{item}</li>;
+    });
+
     return <div>
         React MoveList
-        <ol>
-            {moves.map((move) => (
-                <li key={move.number}>{move.move}</li>
-            ))}
-        </ol>
+        <ol>{move_list}</ol>
     </div>
 }
